75ce3ba1e261e5b5e6c6b3126d1940cdd3b82c00
Paul Miller <paul@paulmillr.com>
2013-01-13 13:49:01 +0200

    Remove `.postinstall` before publish, re-add after publish.
    Closes gh-36.

--- setup.js
+++ setup.js
@@ -2,15 +2,18 @@ var exec = require('child_process').exec;
 var sysPath = require('path');
 var fs = require('fs');
 
-// Cross-platform npm postinstall & test script.
+// Cross-platform node.js postinstall & test script for coffeescript projects.
 
 var mode = process.argv[2];
 
 var fsExists = fs.exists || sysPath.exists;
 
-var execute = function(pathParts, params, callback) {
+var getBinaryPath = function(binary) {
+  return sysPath.join('node_modules', '.bin', binary);
+};
+
+var execute = function(path, params, callback) {
   if (callback == null) callback = function() {};
-  var path = sysPath.join.apply(null, pathParts);
   var command = 'node ' + path + ' ' + params;
   console.log('Executing', command);
   exec(command, function(error, stdout, stderr) {
@@ -19,12 +22,44 @@ var execute = function(pathParts, params, callback) {
   });
 };
 
-if (mode === 'postinstall') {
-  fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
-    if (exists) return;
-    execute(['node_modules', 'coffee-script', 'bin', 'coffee'], '-o lib/ src/');
-  });
-} else if (mode === 'test') {
-  execute(['node_modules', 'mocha', 'bin', 'mocha'],
-    '--compilers coffee:coffee-script --require test/common.js --colors');
+var togglePostinstall = function(add) {
+  var pkg = require('./package.json');
+
+  if (add) {
+    if (!pkg.scripts) pkg.scripts = {};
+    pkg.scripts.postinstall = 'node setup.js test';
+  } else if (pkg.scripts && pkg.scripts.postinstall) {
+    delete pkg.scripts.postinstall;
+  }
+
+  fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
+};
+
+switch (mode) {
+  // Remove `.postinstall` script to prevent stupid npm bugs.
+  case 'prepublish':
+    togglePostinstall(false);
+    execute(getBinaryPath('coffee'), '-o lib/ src/');
+    break;
+
+  // Bring back `.postinstall` script.
+  case 'postpublish':
+    togglePostinstall(true);
+    break;
+
+  // Compile coffeescript for git users.
+  case 'postinstall':
+    fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
+      if (exists) return;
+      execute(getBinaryPath('coffee'), '-o lib/ src/');
+    });
+    break;
+
+  // Run tests.
+  case 'test':
+      execute(
+        getBinaryPath('mocha'),
+        '--compilers coffee:coffee-script --require test/common.js --colors'
+      );
+    break;
 }
