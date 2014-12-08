f26c4d3b411fffb89c4d2098d70484feb6f6b432
Elan Shanker <elan.shanker@gmail.com>
2014-12-08 11:40:13 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,12 @@
 require('./').watch('.', {
   ignored: /node_modules|\.git/,
-  persistent: true
+  persistent: true,
+  //followSymlinks: false,
+  //useFsEvents: false,
+  //usePolling: false
 }).on('all', function(event, path) {
   console.log(event, path);
 }).on('ready', function() {
   console.log('ready');
-});
+})
+//.on('raw', console.log.bind(console, 'RawEvent:'))
