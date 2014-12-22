f04ff030e0df9f6e6fe045cfb3b91c539ac31306
Elan Shanker <elan.shanker@gmail.com>
2014-12-22 15:21:12 -0500

    Fix order

--- test.js
+++ test.js
@@ -32,8 +32,8 @@ describe('chokidar', function() {
     chokidar.watch.should.be.a('function');
   });
 
-  describe('non-polling', runTests.bind(this, {usePolling: false, useFsEvents: false}));
   if (os === 'darwin') describe('fsevents', runTests.bind(this, {useFsEvents: true}));
+  describe('non-polling', runTests.bind(this, {usePolling: false, useFsEvents: false}));
   describe('polling', runTests.bind(this, {usePolling: true}));
 });
 
