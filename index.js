071a0107e3bc142ff3d59828fd81e6ed47ef3d96
Elan Shanker <elan.shanker@gmail.com>
2015-11-07 12:03:08 -0500

    Add safety check before calling cancelWait

--- index.js
+++ index.js
@@ -243,7 +243,9 @@ FSWatcher.prototype._awaitWriteFinish = function(path, threshold, callback) {
         // prevent `unlink` race condition
         this._getWatchedDir(sysPath.dirname(path))
           .remove(sysPath.basename(path));
-        this._pendingWrites[path].cancelWait();
+        if (this._pendingWrites[path]) {  
+          this._pendingWrites[path].cancelWait();
+        }
 
         if (err.code == 'ENOENT') return;
         return callback(err);
