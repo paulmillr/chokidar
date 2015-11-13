f9a0da7e084560d90d756d4716b41960aa2cfb81
Paul Miller <paul@paulmillr.com>
2015-11-13 10:29:33 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,12 +1,12 @@
 require('./').watch('.', {
   ignored: /node_modules|\.git/,
   persistent: true,
-  //followSymlinks: false,
-  //useFsEvents: false,
-  //usePolling: false
+  // followSymlinks: false,
+  // useFsEvents: false,
+  // usePolling: false
 }).on('all', function(event, path) {
   console.log(event, path);
 }).on('ready', function() {
-  console.log('ready');
+  console.log('Ready');
 })
 //.on('raw', console.log.bind(console, 'RawEvent:'))
