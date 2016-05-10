b8f745d5831927be369d8e82431c81898e56f72a
vdiez <diez@outlook.com>
2016-05-10 14:37:00 +0200

    ready event is not prerequisite for awf

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ FSWatcher.prototype._emit = function(event, path, val1, val2, val3) {
     if (event !== 'error') this.emit.apply(this, ['all'].concat(args));
   }.bind(this);
 
-  if (awf && (event === 'add' || event === 'change') && this._readyEmitted) {
+  if (awf && (event === 'add' || event === 'change')) {
     var awfEmit = function(err, stats) {
       if (err) {
         event = args[0] = 'error';
