a0f9e09f64ce7ae70cc6ae1f5615f65cb35f532f
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2024-02-02 15:19:58 -0500

    update fs.FSWatcher types to satisfy node versions >= 16; fixes #1299

--- types/index.d.ts
+++ types/index.d.ts
@@ -60,9 +60,13 @@ export class FSWatcher extends EventEmitter implements fs.FSWatcher {
    */
   on(event: 'ready', listener: () => void): this;
 
-  on(event: 'unlink'|'unlinkDir', listener: (path: string) => void): this;
+  on(event: 'unlink' | 'unlinkDir', listener: (path: string) => void): this;
 
   on(event: string, listener: (...args: any[]) => void): this;
+
+  ref(): this;
+  
+  unref(): this;
 }
 
 export interface WatchOptions {
